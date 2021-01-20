9fea52985bde8e7e65b568fe0530b811d9d729bb
CZUBIX <47487634+CZUBIX@users.noreply.github.com>
2021-01-20 14:45:27 +0100

    Update dzawaskrypt.js

--- cenzurabeta/website/static/js/dzawaskrypt.js
+++ cenzurabeta/website/static/js/dzawaskrypt.js
@@ -12,13 +12,13 @@ if (typeof window.orientation !== "undefined") {
     video.style.display = "none";
     logo.style.width = "20px";
     logo.style.height = "20px";
-    opis.style.fontSize = "12px";
-    title.style.fontSize = "24px";
+    opis.style.fontSize = "14px";
+    title.style.fontSize = "26px";
 
     for (var i = 0; i < buttons.length; i++) {
         buttons[i].style.fontSize = "10px";
-        buttons[i].style.width = "80px";
-        buttons[i].style.height = "20px";
+        buttons[i].style.width = "82px";
+        buttons[i].style.height = "22px";
     }
 
     buttons[2].href = "/sourcecode";
