7f8ebfbf0efff8ddb23e91dd853fd5d10ffa6422
CZUBIX <47487634+CZUBIX@users.noreply.github.com>
2021-02-10 17:56:59 +0100

    Update inne.py

--- cenzurabeta/bot/inne.py
+++ cenzurabeta/bot/inne.py
@@ -1,5 +1,6 @@
 import functions
 import handler
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 
 def load(gateway, discord):
     @gateway.command(description="Pokazuje liste ostatnich usuniętych wiadomości", usage="snipe", category="Inne", _default=True)
@@ -173,191 +174,187 @@ def load(gateway, discord):
 
     @gateway.command(description="Profile", usage="profile", category="Inne", _default=True)
     def profile(ctx):
-        try:
-            if not ctx.args:
+        if not ctx.args:
+            return discord.create_message(ctx.data["channel_id"], {
+                "embed": {
+                    "title": "Komendy profile:",
+                    "description": "> `profile view [osoba]`, `profile set`, `profile remove`",
+                    "color": 0xe74c3c
+                }
+            })
+
+        user = ctx.data["author"]["id"]
+        users = functions.read_json("users")
+
+        if not "profile" in users[user]:
+            users[user]["profile"] = {}
+            functions.write_json("users", users)
+
+        if ctx.args[0] == "view":
+            if not ctx.data["mentions"]:
+                user = ctx.data["author"]
+            else:
+                user = ctx.data["mentions"][0]
+                if not "profile" in users[user["id"]]:
+                    users[user["id"]]["profile"] = {}
+                    functions.write_json("users", users)
+
+            name = "nie podano" if not "name" in users[user["id"]]["profile"] else users[user["id"]]["profile"]["name"]
+            gender = "nie podano" if not "gender" in users[user["id"]]["profile"] else users[user["id"]]["profile"]["gender"]
+            age = "nie podano" if not "age" in users[user["id"]]["profile"] else users[user["id"]]["profile"]["age"]
+            orientation = "nie podano" if not "orientation" in users[user["id"]]["profile"] else users[user["id"]]["profile"]["orientation"]
+
+            discord.create_message(ctx.data["channel_id"], {
+                "embed": {
+                    "title": f"Profil użytkownika {user['username']}:",
+                    "color": 0xe74c3c,
+                    "fields": [
+                        {
+                            "name": "Imie:",
+                            "value": name,
+                            "inline": False
+                        },
+                        {
+                            "name": "Płeć:",
+                            "value": gender,
+                            "inline": False
+                        },
+                        {
+                            "name": "Wiek:",
+                            "value": age,
+                            "inline": False
+                        },
+                        {
+                            "name": "Orientacja:",
+                            "value": orientation,
+                            "inline": False
+                        }
+                    ],
+                    "thumbnail": {
+                        "url": f"http://cdn.discordapp.com/avatars/{user['id']}/{user['avatar']}.png?size=2048"
+                    },
+                    "footer": {
+                        "text": f"Wywołane przez {ctx.data['author']['id']}"
+                    }
+                }
+            })
+        
+        elif ctx.args[0] == "set":
+            if not ctx.args[1:]:
                 return discord.create_message(ctx.data["channel_id"], {
                     "embed": {
-                        "title": "Komendy profile:",
-                        "description": "> `profile view [osoba]`, `profile set`, `profile remove`",
+                        "title": "Komendy profile set:",
+                        "description": "> `profile set name (imie)`, `profile set gender (m/k)`, `profile set age (wiek)`, `profile set orientation (hetero/bi/homo)`",
                         "color": 0xe74c3c
                     }
                 })
 
-            user = ctx.data["author"]["id"]
-            users = functions.read_json("users")
+            if ctx.args[1] == "name":
+                if not len(ctx.args) == 3:
+                    return handler.error_handler(ctx, "arguments", "profile set name (imie)")
 
-            if not "profile" in users[user]:
-                users[user]["profile"] = {}
-                functions.write_json("users", users)
+                if not len(ctx.args[2]) < 10:
+                    return handler.error_handler(ctx, "toolong", 10)
 
-            if ctx.args[0] == "view":
-                if not ctx.data["mentions"]:
-                    user = ctx.data["author"]
-                else:
-                    user = ctx.data["mentions"][0]
-                    if not "profile" in users[user["id"]]:
-                        users[user["id"]]["profile"] = {}
-                        functions.write_json("users", users)
-
-                name = "nie podano" if not "name" in users[user["id"]]["profile"] else users[user["id"]]["profile"]["name"]
-                gender = "nie podano" if not "gender" in users[user["id"]]["profile"] else users[user["id"]]["profile"]["gender"]
-                age = "nie podano" if not "age" in users[user["id"]]["profile"] else users[user["id"]]["profile"]["age"]
-                orientation = "nie podano" if not "orientation" in users[user["id"]]["profile"] else users[user["id"]]["profile"]["orientation"]
+                users[user]["profile"]["name"] = ctx.args[2]
 
                 discord.create_message(ctx.data["channel_id"], {
+                    "content": "Ustawiono imie"
+                })
+
+            elif ctx.args[1] == "gender":
+                if not len(ctx.args) == 3:
+                    return handler.error_handler(ctx, "arguments", "profile set gender (m/k)")
+
+                male = ["M", "MEZCZYZNA"]
+                female = ["K", "KOBIETA"]
+                available = male + female
+
+                if ctx.args[2].upper() in available:
+                    if ctx.args[2].upper() in male:
+                        users[user]["profile"]["gender"] = "mężczyzna"
+                    elif ctx.args[2].upper() in female:
+                        users[user]["profile"]["gender"] = "kobieta"
+                else:
+                    return handler.error_handler(ctx, "arguments", "profile set gender (m/k)")
+
+                discord.create_message(ctx.data["channel_id"], {
+                    "content": "Ustawiono płeć"
+                })
+
+            elif ctx.args[1] == "age":
+                if not len(ctx.args) == 3:
+                    return handler.error_handler(ctx, "arguments", "profile set age (wiek)")
+
+                try:
+                    if int(ctx.args[2]) > 100:
+                        return discord.create_message(ctx.data["channel_id"], {
+                            "content": "Za dużo"
+                        })
+                    elif int(ctx.args[2]) < 13:
+                        return discord.create_message(ctx.data["channel_id"], {
+                            "content": "Za mało"
+                        })
+                except:
+                    return handler.error_handler(ctx, "arguments", "profile set age (wiek)")
+
+                users[user]["profile"]["age"] = ctx.args[2]
+
+                discord.create_message(ctx.data["channel_id"], {
+                    "content": "Ustawiono wiek"
+                })
+
+            elif ctx.args[1] == "orientation":
+                if not len(ctx.args) == 3:
+                    return handler.error_handler(ctx, "arguments", "profile set orientation (hetero/bi/homo)")
+
+                available = ["HETERO", "BI", "HOMO"]
+
+                if ctx.args[2].upper() in available:
+                    users[user]["profile"]["orientation"] = ctx.args[2].lower()
+                else:
+                    return handler.error_handler(ctx, "arguments", "profile set orientation (hetero/bi/homo)")
+
+                discord.create_message(ctx.data["channel_id"], {
+                    "content": "Ustawiono orientacje"
+                })
+
+        elif ctx.args[0] == "remove":
+            if not len(ctx.args) <= 3:
+                return discord.create_message(ctx.data["channel_id"], {
                     "embed": {
-                        "title": f"Profil użytkownika {user['username']}:",
-                        "color": 0xe74c3c,
-                        "fields": [
-                            {
-                                "name": "Imie:",
-                                "value": name,
-                                "inline": False
-                            },
-                            {
-                                "name": "Płeć:",
-                                "value": gender,
-                                "inline": False
-                            },
-                            {
-                                "name": "Wiek:",
-                                "value": age,
-                                "inline": False
-                            },
-                            {
-                                "name": "Orientacja:",
-                                "value": orientation,
-                                "inline": False
-                            }
-                        ],
-                        "thumbnail": {
-                            "url": f"http://cdn.discordapp.com/avatars/{user['id']}/{user['avatar']}.png?size=2048"
-                        },
-                        "footer": {
-                            "text": f"Wywołane przez {ctx.data['author']['id']}"
-                        }
+                        "title": "Komendy profile remove:",
+                        "description": "> `profile remove name`, `profile remove gender`, `profile remove age`, `profile remove orientation`",
+                        "color": 0xe74c3c
                     }
                 })
-            
-            elif ctx.args[0] == "set":
-                if not ctx.args[1:]:
-                    return discord.create_message(ctx.data["channel_id"], {
-                        "embed": {
-                            "title": "Komendy profile set:",
-                            "description": "> `profile set name (imie)`, `profile set gender (m/k)`, `profile set age (wiek)`, `profile set orientation (hetero/bi/homo)`",
-                            "color": 0xe74c3c
-                        }
-                    })
 
-                if ctx.args[1] == "name":
-                    if not len(ctx.args) == 3:
-                        return handler.error_handler(ctx, "arguments", "profile set name (imie)")
+            if ctx.args[1] == "name":
+                del users[user]["profile"]["name"]
 
-                    if not len(ctx.args[2]) < 10:
-                        return handler.error_handler(ctx, "toolong", 10)
+                discord.create_message(ctx.data["channel_id"], {
+                    "content": "Usunięto imie z twojego profilu"
+                })
 
-                    users[user]["profile"]["name"] = ctx.args[2]
+            elif ctx.args[1] == "gender":
+                del users[user]["profile"]["gender"]
 
-                    discord.create_message(ctx.data["channel_id"], {
-                        "content": "Ustawiono imie"
-                    })
+                discord.create_message(ctx.data["channel_id"], {
+                    "content": "Usunięto płeć z twojego profilu"
+                })
 
-                elif ctx.args[1] == "gender":
-                    if not len(ctx.args) == 3:
-                        return handler.error_handler(ctx, "arguments", "profile set gender (m/k)")
+            elif ctx.args[1] == "age":
+                del users[user]["profile"]["age"]
 
-                    male = ["M", "MEZCZYZNA"]
-                    female = ["K", "KOBIETA"]
-                    available = male + female
+                discord.create_message(ctx.data["channel_id"], {
+                    "content": "Usunięto wiek z twojego profilu"
+                })
 
-                    if ctx.args[2].upper() in available:
-                        if ctx.args[2].upper() in male:
-                            users[user]["profile"]["gender"] = "mężczyzna"
-                        elif ctx.args[2].upper() in female:
-                            users[user]["profile"]["gender"] = "kobieta"
-                    else:
-                        return handler.error_handler(ctx, "arguments", "profile set gender (m/k)")
+            elif ctx.args[1] == "orientation":
+                del users[user]["profile"]["orientation"]
 
-                    discord.create_message(ctx.data["channel_id"], {
-                        "content": "Ustawiono płeć"
-                    })
+                discord.create_message(ctx.data["channel_id"], {
+                    "content": "Usunięto orientacje z twojego profilu"
+                })
 
-                elif ctx.args[1] == "age":
-                    if not len(ctx.args) == 3:
-                        return handler.error_handler(ctx, "arguments", "profile set age (wiek)")
-
-                    try:
-                        if int(ctx.args[2]) > 100:
-                            return discord.create_message(ctx.data["channel_id"], {
-                                "content": "Za dużo"
-                            })
-                        elif int(ctx.args[2]) < 13:
-                            return discord.create_message(ctx.data["channel_id"], {
-                                "content": "Za mało"
-                            })
-                    except:
-                        return handler.error_handler(ctx, "arguments", "profile set age (wiek)")
-
-                    users[user]["profile"]["age"] = ctx.args[2]
-
-                    discord.create_message(ctx.data["channel_id"], {
-                        "content": "Ustawiono wiek"
-                    })
-
-                elif ctx.args[1] == "orientation":
-                    if not len(ctx.args) == 3:
-                        return handler.error_handler(ctx, "arguments", "profile set orientation (hetero/bi/homo)")
-
-                    available = ["HETERO", "BI", "HOMO"]
-
-                    if ctx.args[2].upper() in available:
-                        users[user]["profile"]["orientation"] = ctx.args[2].lower()
-                    else:
-                        return handler.error_handler(ctx, "arguments", "profile set orientation (hetero/bi/homo)")
-
-                    discord.create_message(ctx.data["channel_id"], {
-                        "content": "Ustawiono orientacje"
-                    })
-
-            elif ctx.args[0] == "remove":
-                if not len(ctx.args) <= 3:
-                    return discord.create_message(ctx.data["channel_id"], {
-                        "embed": {
-                            "title": "Komendy profile remove:",
-                            "description": "> `profile remove name`, `profile remove gender`, `profile remove age`, `profile remove orientation`",
-                            "color": 0xe74c3c
-                        }
-                    })
-
-                if ctx.args[1] == "name":
-                    del users[user]["profile"]["name"]
-
-                    discord.create_message(ctx.data["channel_id"], {
-                        "content": "Usunięto imie z twojego profilu"
-                    })
-
-                elif ctx.args[1] == "gender":
-                    del users[user]["profile"]["gender"]
-
-                    discord.create_message(ctx.data["channel_id"], {
-                        "content": "Usunięto płeć z twojego profilu"
-                    })
-
-                elif ctx.args[1] == "age":
-                    del users[user]["profile"]["age"]
-
-                    discord.create_message(ctx.data["channel_id"], {
-                        "content": "Usunięto wiek z twojego profilu"
-                    })
-
-                elif ctx.args[1] == "orientation":
-                    del users[user]["profile"]["orientation"]
-
-                    discord.create_message(ctx.data["channel_id"], {
-                        "content": "Usunięto orientacje z twojego profilu"
-                    })
-
-            functions.write_json("users", users)
-        except:
-            import traceback
-            traceback.print_exc()
+        functions.write_json("users", users)
